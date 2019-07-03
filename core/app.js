a2e0569c40e3dea53fe92b551a13ff93bb747512
Sumit Parakh <sumit.parakh@amla.io>
2019-07-03 14:18:46 +0530

    minor changes

--- core/app.js
+++ core/app.js
@@ -1,26 +1,24 @@
-"use strict";
-
 function Application() {
-
+    "use strict";
     var app = this;
     this.onBoardObj = new Onboard();
 
     this.start = function () {
         this.initialize();
-    }
+    };
 
     this.initialize = function () {
         document.getElementById('btnGetStarted').addEventListener('click', function (event) {
             app.hideGetStartedContainer();
             app.onBoardObj.initialize(event);
         });
-    }
+    };
 
     this.hideGetStartedContainer = function () {
         document.getElementById('getStartedContainer').classList.add('d-none');
-    }
+    };
 
     this.showGetStartedContainer = function () {
         document.getElementById('getStartedContainer').classList.remove('d-none');
-    }
+    };
 }
